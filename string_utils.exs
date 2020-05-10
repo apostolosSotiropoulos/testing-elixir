a64e58aa363d0618b20712c8eef4940731bcdeec
me <me@example.com>
2020-05-10 14:58:49 +0200

    2 lettered strings take 2

--- string_utils.exs
+++ string_utils.exs
@@ -1,15 +1,8 @@
 defmodule StringUtils do
   def two_lettered_strings(string) do
-    with_trailing_underscore = &(&1 ++ ["_"])
-    list_of_pairs = &(Enum.chunk_every(&1, 2, 2, :discard))
-    to_string = &(List.to_string/1)
-    list_of_strings = &(Enum.map(&1, to_string))
-
-
-    string 
-    |> String.graphemes()
-    |> with_trailing_underscore.()
-    |> list_of_pairs.()
-    |> list_of_strings.()
+    string
+    |> to_char_list()
+    |> Enum.chunk_every(2, 2, '_')
+    |> Enum.map(&to_string/1)
   end
 end
