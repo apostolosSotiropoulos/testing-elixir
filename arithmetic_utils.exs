eb4bd1411bdb962c8acaff3d841fff616b8e08e1
me <me@example.com>
2020-06-06 17:42:01 +0200

    set_of_multiples

--- arithmetic_utils.exs
+++ arithmetic_utils.exs
@@ -7,11 +7,14 @@ defmodule ArithmeticUtils do
     |> digital_root
   end
 
-  def set_of_multiples(n, limit, set \\ MapSet.new()) do
+  def set_of_multiples(n, limit, set, current_multiplier) when n * current_multiplier >= limit, do: set
+  def set_of_multiples(n, limit, set \\ MapSet.new(), current_multiplier \\ 0) do
+    multiplier = current_multiplier + 1
     set
-    |> MapSet.put(n)
-    |> MapSet.put(limit)
+    |> (fn s -> if n * multiplier >= limit, do: s, else: MapSet.put(s, n * multiplier) end).()
+    |> (&set_of_multiples(n, limit, &1, multiplier)).()
   end
 end
 
-IO.puts Enum.sum ArithmeticUtils.set_of_multiples 1, 2
+IO.inspect ArithmeticUtils.set_of_multiples(3, 1)  #MapSet<[]
+IO.inspect ArithmeticUtils.set_of_multiples(3, 10) #MapSet<[3, 6, 9]>
