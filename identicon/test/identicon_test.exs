999f852e73f17d8315e0176611a30dd2f74ecaba
me <me@example.com>
2021-05-06 23:41:11 +0200

    proper testing for identicon

--- identicon/test/identicon_test.exs
+++ identicon/test/identicon_test.exs
@@ -3,8 +3,21 @@ defmodule IdenticonTest do
   doctest Identicon
 
   test "creates a symmetric list of tiles" do
-    identicon = Identicon.create
-    [first, second, _, fourth, fifth] = identicon
+    assert_list_with_symetric_lists_as_elements Identicon.create
+  end
+
+  test "creates a uniq list of tiles every time" do
+    assert 2 == Enum.uniq([Identicon.create, Identicon.create]) |> Enum.count
+  end
+
+  defp assert_list_with_symetric_lists_as_elements([]) do; end
+
+  defp assert_list_with_symetric_lists_as_elements([head | tail]) do
+    assert_symetric_list head
+    assert_list_with_symetric_lists_as_elements tail
+  end
+
+  defp assert_symetric_list([first, second, _, fourth, fifth]) do
     assert first == fifth
     assert second == fourth
   end
